624053f5602003af4c7ed742eaf514b87094697a
iMedia Matt <root@node-1.(none)>
2013-03-29 20:55:36 -0400

    merge

---
+++ app/js/controllers/main.js
@@ -0,0 +1,24 @@
+'use strict';require.config({
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: ['underscore'],
+      exports: 'Backbone'
+    }
+  },
+  paths: {
+    underscore: '../vendors/underscore',
+    backbone: '../vendors/backbone',
+    route: 'route',
+    uri: '../vendors/URIjs',
+    controllers: '../controllers/',
+    models: '../models/',
+    views: '../views/'
+  }
+});
+
+require(['route'], function(Router) {
+  return console.log(Router);
+});
